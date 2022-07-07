01242b65228c9d806871cb24a57ce0b94d5c57a1
Ema Čampa <emacampa3@gmail.com>
2022-07-07 10:21:53 +0200

    some changes

--- hello.py
+++ hello.py
@@ -627,3 +627,52 @@ print(len(albums)) # 3
 
 for name, artist, year in albums: # unpacking tuples
   print("Album: {}, Artist: {}, Year: {}".format(name, artist, year))
+
+# NESTED DATA
+albums = [
+("Welcome to my Nightmare", "Alice Cooper", 1975,
+  [
+    (1, "Welcome to my Nightmare"),
+    (2, "Devil's Food"),
+    (3, "The Black Widow"),
+    (4, "Some Folks"),
+    (5, "Only Women Bleed"),
+  ]
+  ),
+("Bad Company", "Bad Company", 1974,
+  [
+    (1, "Can't Get Enough"),
+    (2, "Rock Steady"),
+    (3, "Ready for Love"),
+    (4, "Don't Let Me Down"),
+    (5, "Bad Company"),
+    (6, "The Way I Choose"),
+    (7, "Movin' On"),
+    (8, "Seagull"),
+  ]
+  ),
+("Nightflight", "Budgie", 1981,
+  [
+    (1, "I Turned to Stone"),
+    (2, "Keeping a Rendezvous"),
+    (3, "Reaper of the Glory"),
+    (4, "She Used Me Up"),
+  ]
+)
+]
+for name, artist, year, songs in albums:
+  print("Album: {}, Artist: {}, Year: {}, Songs: {}".format(name, artist, year, songs))
+
+album = albums[2]
+print(album) # the 3rd album
+
+songs = album[3]
+print(songs) # list of songs in 3rd album
+
+song = songs[1]
+print(song) # 2nd song of 3rd album 
+print(song[1]) # title of 2nd song of the 3rd album
+
+# Song: Reaper of the Glory
+reaper = albums[2][3][2][1]
+print(reaper)
